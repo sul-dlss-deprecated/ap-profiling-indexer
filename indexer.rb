298cdd31c7d41d77e244697ddc0f39e487815ace
Naomi Dushay <ndushay@stanford.edu>
2013-02-08 14:19:48 -0800

    added blacklist and whitelist capabilities

--- indexer.rb
+++ indexer.rb
@@ -37,22 +37,15 @@ class Indexer
   
   # per this Indexer's config options 
   #  harvest the druids via OAI
-  #   create a Solr document for each druid suitable for SearchWorks
-  #   write the result to the SearchWorks Solr index
+  #   create Solr profiling documents for each druid
+  #   write the result to the Solr index
   def harvest_and_index
-    druids.each { |druid|  
-      vol = volume(druid)
-#      spd = SaxProfilingDocument.new(solr_client, druid, vol, collection, logger)
-      spd = ApTeiProfilingDocument.new(solr_client, druid, vol, collection, logger)
-      parser = Nokogiri::XML::SAX::Parser.new(spd)
-      tei_xml = tei(druid)
-      logger.info("About to parse #{druid} (#{vol})")
-      parser.parse(tei_xml)
-      logger.info("Finished parsing #{druid}")
-      solr_client.commit
-      logger.info("Sent commit to Solr")
-    }
-    solr_client.commit
+    if whitelist.empty?
+      druids.each { |druid| profile druid }
+    else
+      whitelist.each { |druid| profile druid }
+    end
+    logger.info("Finished processing: final Solr commit returned.")
   end
   
   # return Array of druids contained in the OAI harvest indicated by OAI params in yml configuration file
@@ -83,12 +76,51 @@ class Indexer
     @solr_client ||= RSolr.connect(config.solr.to_hash)
   end
 
+  # @return an Array of druids ('oo000oo0000') that should NOT be processed
+  def blacklist
+    # avoid trying to load the file multiple times
+    if !@blacklist && !@loaded_blacklist
+      @blacklist = load_blacklist(config.blacklist) if config.blacklist
+    end
+    @blacklist ||= []
+  end
+  
+  # @return an Array of druids ('oo000oo0000') that should NOT be processed
+  def whitelist
+    # avoid trying to load the file multiple times
+    if !@whitelist && !@loaded_whitelist
+      @whitelist = load_whitelist(config.whitelist) if config.whitelist
+    end
+    @whitelist ||= []
+  end
+  
   protected #---------------------------------------------------------------------
 
   def harvestdor_client
     @harvestdor_client ||= Harvestdor::Client.new({:config_yml_path => @yml_path})
   end
   
+  # create Solr doc for the druid and add it to Solr, unless it is on the blacklist.  
+  #  NOTE: no Solr commit performed
+  def profile druid
+    if blacklist.include?(druid)
+      logger.info("Druid #{druid} is on the blacklist and will not be processed")
+    else
+      vol = volume(druid)
+#       spd = SaxProfilingDocument.new(solr_client, druid, vol, collection, logger)
+      spd = ApTeiProfilingDocument.new(solr_client, druid, vol, collection, logger)
+      parser = Nokogiri::XML::SAX::Parser.new(spd)
+      tei_xml = tei(druid)
+      logger.info("About to parse #{druid} (#{vol})")
+      parser.parse(tei_xml)
+      logger.info("Finished parsing #{druid}")
+      solr_client.commit
+      logger.info("Sent commit to Solr")
+      # TODO: update DOR object's workflow datastream??
+    end
+  end
+  
+
   # Global, memoized, lazy initialized instance of a logger
   # @param String directory for to get log file
   # @param String name of log file
@@ -97,4 +129,43 @@ class Indexer
     @logger ||= Logger.new(File.join(log_dir, log_name), 'daily')
   end
     
+  # populate @blacklist as an Array of druids ('oo000oo0000') that will NOT be processed
+  #  by reading the File at the indicated path
+  # @param path - path of file containing a list of druids
+  def load_blacklist path
+    if path && !@loaded_blacklist
+      @loaded_blacklist = true
+      @blacklist = load_id_list path
+    end
+  end
+    
+  # populate @blacklist as an Array of druids ('oo000oo0000') that WILL be processed
+  #  (unless a druid is also on the blacklist)
+  #  by reading the File at the indicated path
+  # @param path - path of file containing a list of druids
+  def load_whitelist path
+    if path && !@loaded_whitelist
+      @loaded_whitelist = true
+      @whitelist = load_id_list path
+    end
+  end
+    
+  # return an Array of druids ('oo000oo0000')
+  #   populated by reading the File at the indicated path
+  # @param path - path of file containing a list of druids
+  # @return an Array of druids
+  def load_id_list path
+    if path 
+      list = []
+      f = File.open(path).each_line { |line|
+        list << line.gsub(/\s+/, '') if !line.gsub(/\s+/, '').empty?
+      }
+      list
+    end
+  rescue
+    msg = "Unable to find list of druids at " + path
+    logger.fatal msg
+    raise msg
+  end
+
 end
--- spec/unit/indexer_spec.rb
+++ spec/unit/indexer_spec.rb
@@ -9,6 +9,8 @@ describe Indexer do
     @yaml = YAML.load_file(@config_yml_path)
     @hdor_client = @indexer.send(:harvestdor_client)
     @fake_druid = 'oo000oo0000'
+    @blacklist_path = File.join(File.dirname(__FILE__), "../config/ap_blacklist.txt")
+    @whitelist_path = File.join(File.dirname(__FILE__), "../config/ap_whitelist.txt")
     @im_ng = Nokogiri::XML('<identityMetadata><objectLabel>Volume 36</objectLabel></identityMetadata>')
     @url = "#{@yaml['stacks']}/file/druid:#{@fake_druid}/#{@fake_druid}.xml"
   end
@@ -35,7 +37,48 @@ describe Indexer do
       hdor_client.should_receive(:identity_metadata).with('3').and_return(@im_ng)
       OpenURI.should_receive(:open_uri).with(any_args).exactly(3).times.and_return('<TEI.2/>')
       indexer.solr_client.should_receive(:add).with(hash_including(:id, :volume_ssi => 'Volume 36')).exactly(3).times
-      indexer.solr_client.should_receive(:commit).at_least(3).times
+      indexer.solr_client.should_receive(:commit).exactly(3).times
+      indexer.harvest_and_index
+    end
+    it "should not process druids in blacklist" do
+      indexer = Indexer.new(@config_yml_path, {:blacklist => @blacklist_path})
+      hdor_client = indexer.send(:harvestdor_client)
+      indexer.stub(:solr_doc).with('oo000oo0000').and_return({:id => 'oo000oo0000', :field => 'val' })
+      indexer.stub(:solr_doc).with('oo111oo1111').and_return({:id => 'oo111oo1111', :field => 'val' })
+      indexer.stub(:solr_doc).with('oo222oo2222').and_return({:id => 'oo222oo2222', :field => 'val' })
+      indexer.stub(:solr_doc).with('oo333oo3333').and_return({:id => 'oo333oo3333', :field => 'val' })
+      hdor_client.should_receive(:druids_via_oai).and_return(['oo000oo0000', 'oo111oo1111', 'oo222oo2222', 'oo333oo3333'])
+      hdor_client.should_receive(:identity_metadata).with(anything).twice.and_return(@im_ng)
+      OpenURI.should_receive(:open_uri).with(any_args).twice.and_return('<TEI.2/>')
+      indexer.solr_client.should_receive(:add).with(hash_including({:id => 'oo000oo0000'}))
+      indexer.solr_client.should_not_receive(:add).with(hash_including({:id => 'oo111oo1111'}))
+      indexer.solr_client.should_not_receive(:add).with(hash_including({:id => 'oo222oo2222'}))
+      indexer.solr_client.should_receive(:add).with(hash_including({:id => 'oo333oo3333'}))
+      indexer.solr_client.should_receive(:commit).twice
+      indexer.harvest_and_index
+    end
+    it "should only process druids in whitelist if it exists" do
+      indexer = Indexer.new(@config_yml_path, {:whitelist => @whitelist_path})
+      hdor_client = indexer.send(:harvestdor_client)
+      indexer.stub(:solr_doc).with('oo000oo0000').and_return({:id => 'oo000oo0000', :field => 'val' })
+      indexer.stub(:solr_doc).with('oo222oo2222').and_return({:id => 'oo222oo2222', :field => 'val' })
+      hdor_client.should_not_receive(:druids_via_oai)
+      hdor_client.should_receive(:identity_metadata).with(anything).twice.and_return(@im_ng)
+      OpenURI.should_receive(:open_uri).with(any_args).twice.and_return('<TEI.2/>')
+      indexer.solr_client.should_receive(:add).with(hash_including({:id => 'oo000oo0000'}))
+      indexer.solr_client.should_receive(:add).with(hash_including({:id => 'oo222oo2222'}))
+      indexer.solr_client.should_receive(:commit).twice
+      indexer.harvest_and_index
+    end
+    it "should not process druids if it is in both blacklist and whitelist" do
+      indexer = Indexer.new(@config_yml_path, {:blacklist => @blacklist_path, :whitelist => @whitelist_path})
+      hdor_client = indexer.send(:harvestdor_client)
+      indexer.stub(:solr_doc).with('oo000oo0000').and_return({:id => 'oo000oo0000', :field => 'val' })
+      hdor_client.should_not_receive(:druids_via_oai)
+      hdor_client.should_receive(:identity_metadata).with(anything).once.and_return(@im_ng)
+      OpenURI.should_receive(:open_uri).with(any_args).once.and_return('<TEI.2/>')
+      indexer.solr_client.should_receive(:add).with(hash_including({:id => 'oo000oo0000'}))
+      indexer.solr_client.should_receive(:commit).once
       indexer.harvest_and_index
     end
   end
@@ -85,6 +128,88 @@ describe Indexer do
     @indexer.druids
   end
     
+  context "blacklist" do
+    it "should be an Array with an entry for each non-empty line in the file" do
+      @indexer.send(:load_blacklist, @blacklist_path)
+      @indexer.send(:blacklist).should be_an_instance_of(Array)
+      @indexer.send(:blacklist).size.should == 2
+    end
+    it "should be empty Array if there was no blacklist config setting" do
+      indexer = Indexer.new(@config_yml_path)
+      indexer.stub(:solr_doc).and_return(@doc_hash)
+      indexer.send(:blacklist).should == []
+    end
+    context "load_blacklist" do
+      it "should not be called if there was no blacklist config setting" do
+        indexer = Indexer.new(@config_yml_path)
+
+        indexer.should_not_receive(:load_blacklist)
+
+        indexer.stub(:solr_doc).and_return(@doc_hash)
+        hdor_client = indexer.send(:harvestdor_client)
+        hdor_client.should_receive(:druids_via_oai).and_return([@fake_druid])
+        hdor_client.should_receive(:identity_metadata).with(anything).and_return(@im_ng)
+        OpenURI.should_receive(:open_uri).with(any_args).and_return('<TEI.2/>')
+        indexer.solr_client.should_receive(:add)
+        indexer.solr_client.should_receive(:commit)
+        indexer.harvest_and_index
+      end
+      it "should only try to load a blacklist once" do
+        indexer = Indexer.new(@config_yml_path, {:blacklist => @blacklist_path})
+        indexer.send(:blacklist)
+        File.any_instance.should_not_receive(:open)
+        indexer.send(:blacklist)
+      end
+      it "should log an error message and throw RuntimeError if it can't find the indicated blacklist file" do
+        exp_msg = 'Unable to find list of druids at bad_path'
+        indexer = Indexer.new(@config_yml_path, {:blacklist => 'bad_path'})
+        indexer.logger.should_receive(:fatal).with(exp_msg)
+        expect { indexer.send(:load_blacklist, 'bad_path') }.to raise_error(exp_msg)
+      end   
+    end
+  end # blacklist
+  
+  context "whitelist" do
+    it "should be an Array with an entry for each non-empty line in the file" do
+      @indexer.send(:load_whitelist, @whitelist_path)
+      @indexer.send(:whitelist).should be_an_instance_of(Array)
+      @indexer.send(:whitelist).size.should == 2
+    end
+    it "should be empty Array if there was no whitelist config setting" do
+      indexer = Indexer.new(@config_yml_path)
+      indexer.stub(:solr_doc).and_return(@doc_hash)
+      indexer.send(:whitelist).should == []
+    end
+    context "load_whitelist" do
+      it "should not be called if there was no whitelist config setting" do
+        indexer = Indexer.new(@config_yml_path)
+
+        indexer.should_not_receive(:load_whitelist)
+
+        indexer.stub(:solr_doc).and_return(@doc_hash)
+        hdor_client = indexer.send(:harvestdor_client)
+        hdor_client.should_receive(:druids_via_oai).and_return([@fake_druid])
+        hdor_client.should_receive(:identity_metadata).with(anything).and_return(@im_ng)
+        OpenURI.should_receive(:open_uri).with(any_args).and_return('<TEI.2/>')
+        indexer.solr_client.should_receive(:add)
+        indexer.solr_client.should_receive(:commit)
+        indexer.harvest_and_index
+      end
+      it "should only try to load a whitelist once" do
+        indexer = Indexer.new(@config_yml_path, {:whitelist => @whitelist_path})
+        indexer.send(:whitelist)
+        File.any_instance.should_not_receive(:open)
+        indexer.send(:whitelist)
+      end
+      it "should log an error message and throw RuntimeError if it can't find the indicated whitelist file" do
+        exp_msg = 'Unable to find list of druids at bad_path'
+        indexer = Indexer.new(@config_yml_path, {:whitelist => 'bad_path'})
+        indexer.logger.should_receive(:fatal).with(exp_msg)
+        expect { indexer.send(:load_whitelist, 'bad_path') }.to raise_error(exp_msg)
+      end   
+    end
+  end # whitelist
+
   it "solr_client should initialize the rsolr client using the options from the config" do
     indexer = Indexer.new(nil, Confstruct::Configuration.new(:solr => { :url => 'http://localhost:2345', :a => 1 }) )
     RSolr.should_receive(:connect).with(hash_including(:a => 1, :url => 'http://localhost:2345')).and_return('foo')
